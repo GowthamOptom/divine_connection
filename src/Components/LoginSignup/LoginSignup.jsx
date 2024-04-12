55f0f18a18afb7d4c881e08718e0c2c293c3f289
Gowtham P <106870998+GowthamOptom@users.noreply.github.com>
2024-04-12 10:17:44 +0530

    react app created and named divine_connection

---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './LoginSignup.css'
+
+export const LoginSignup = () => {
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  )
+}
